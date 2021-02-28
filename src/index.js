c13f6b9901bb1a236854e78fa34e5e32cca3ad6a
aditya-yadav08 <aditya.yadav2708@gmail.com>
2021-02-28 14:09:58 +0530

    Spotify Clone

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; 
 import reportWebVitals from './reportWebVitals';
+import { DataLayer } from "./components/DataLayer";
+import reducer, { initialState } from "./components/reducer";
 
 ReactDOM.render(
   <React.StrictMode>
+    <DataLayer initialState={initialState} reducer={reducer}> 
     <App />
+    </DataLayer>
   </React.StrictMode>,
   document.getElementById('root')
 );
